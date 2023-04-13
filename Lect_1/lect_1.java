63cc1886b0d59170bed50e34bb9d6040acaeda2f
dkob1996 <dkob1996@gmail.com>
2023-04-14 01:36:37 +0600

    redirect repo

---
+++ Lect_1/lect_1.java
@@ -0,0 +1,19 @@
+/**
+ *  comment 1
+ */
+// comment 2
+
+public class lect_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        String s;
+        s= "yo";
+        System.out.println(s);
+
+        short age =10;
+        int salary = 123456;
+        System.out.println(age); //10
+        System.out.println(salary);  //123456
+    }
+}
