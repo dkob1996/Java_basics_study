4148ff6e2cd6328c98dcb23d85788e5626fbb688
dkob1996 <dkob1996@gmail.com>
2023-04-23 02:01:11 +0600

    fix lect3

--- Lect3/task7/program.java
+++ Lect3/task7/program.java
@@ -5,10 +5,10 @@ import java.util.*;
 
 public class program {
     public static void main(String[] args) {
-        Character value = null;
+        //Character value = null;
         List<Character> list1 = List.of('S', 'e', 'r', 'g', 'e', 'y');
         System.out.println(list1);
         List<Character> list2 = List.copyOf(list1);
-
+        System.out.println(list2);
     }
 }
