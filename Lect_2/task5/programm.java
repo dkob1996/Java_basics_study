299698d8fa539e9722e67d01558e9956a607c9c2
dkob1996 <dkob1996@gmail.com>
2023-05-07 15:01:48 +0600

    done lect2

---
+++ Lect_2/task5/programm.java
@@ -0,0 +1,25 @@
+package Lect_2.task5;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class programm {
+    public static void main(String[] args) throws IOException {
+    
+        Logger logger = Logger.getLogger(programm.class.getName());
+        //ConsoleHandler ch = new ConsoleHandler();
+        FileHandler fh = new FileHandler("log.txt");
+        //logger.addHandler(ch);
+        logger.addHandler(fh);
+        
+        SimpleFormatter sFormat = new SimpleFormatter();
+        //XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(sFormat);
+        //fh.setFormatter(xml);
+        
+        //logger.setLevel(Level.INFO);
+        logger.log(Level.WARNING, "Тестовое логирование 1");
+        logger.info("Тестовое логирование 2");
+
+    }
+}
